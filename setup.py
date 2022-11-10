2aaaf1009b3e15491e5b3e2d5d3c0cc3afead80a
Patrick Bray <patrickbray17@gmail.com>
2022-11-10 18:07:46 -0500

    run black

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Providing insights to accident risk by location',
-    author='Patrick Bray',
-    license='MIT',
+    version="0.1.0",
+    description="Providing insights to accident risk by location",
+    author="Patrick Bray",
+    license="MIT",
 )
