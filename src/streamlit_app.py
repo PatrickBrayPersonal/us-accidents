8f6e075a389c165d0d16465410f6bc08300bba28
Patrick Bray <patrickbray17@gmail.com>
2022-11-13 15:38:34 -0500

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,1 +1,18 @@
 import streamlit as st
+from src.visualization.visualize import kde
+import os
+import dask.dataframe as dd
+from dotenv import find_dotenv, load_dotenv
+from pathlib import Path
+import plotly.express as px
+
+load_dotenv(find_dotenv())
+project_dir = Path(os.getenv("WORKSPACE"))
+
+accs = dd.read_parquet(
+    "data/processed/us-accidents.parquet",
+    parse_dates=["Start_Time", "End_Time"],
+)
+
+accs = accs[accs["State"] == "DC"]
+st.plotly_chart(kde(accs))
