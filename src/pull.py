bf1a4b9afb81236fd336d72d6e3ff48c1f078128
Patrick Bray <patrickbray17@gmail.com>
2022-11-17 22:29:06 -0500

    allow for db pull

---
+++ src/pull.py
@@ -0,0 +1,20 @@
+from google.cloud import bigquery
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+client = bigquery.Client()
+
+
+def get_accidents(lat, lng, box_sz) -> float:
+    query = f"""
+        SELECT Start_Lat, Start_Lng 
+        FROM `dvahw3-365900.us_accidents.accidents` 
+        WHERE Start_Lat BETWEEN {lat - box_sz/2} AND {lat + box_sz/2} AND Start_Lng BETWEEN {lng - box_sz/2} AND {lng + box_sz/2}
+        LIMIT 1000
+    """
+    return client.query(query).to_dataframe()
+
+
+if __name__ == "__main__":
+    get_accidents(40, -70, 10)
--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -4,8 +4,29 @@ import os
 import dask.dataframe as dd
 from dotenv import find_dotenv, load_dotenv
 from pathlib import Path
-import plotly.express as px
+import requests
 
+
+
+## FRONTEND ##
+st.title("Accidents in the United States")
+st.subheader("Visualize traffic safety in any neighborhood")
+
+address = st.sidebar.text_input('Lookup your address', key='address')
+radius = st.sidebar.text_input('Search radius', key='radius')
+
+def get_lat_lon():
+    placesapikey = os.getenv("API_KEY")
+    url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + st.session_state.address.replace(" ", "%2C" ) + "&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=" + placesapikey
+    response = requests.request("GET", url, headers={}, data={})
+    result = response.json()['candidates'][0]
+    print(result)
+    return result['geometry']['location']['lat'], result['geometry']['location']['lng']
+lat_lon = st.sidebar.button('Find Address', on_click=get_lat_lon)
+
+
+
+## VISUALIZATION ##
 load_dotenv(find_dotenv())
 project_dir = Path(os.getenv("WORKSPACE"))
 
